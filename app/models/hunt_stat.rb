02b4fc180afdf315b27cc4adbc079c5517da76d1
grantfogle <grantjfogle@gmail.com>
2026-01-07 20:41:40 -0700

    cleaned up form fields for dashboard

--- app/models/hunt_stat.rb
+++ app/models/hunt_stat.rb
@@ -1,10 +1,16 @@
 class HuntStat < ApplicationRecord
-    SPECIES_OPTIONS = ["ELK"].freeze
-    SEX_OPTIONS = ["M", "F", "E"].freeze
-    METHOD_OPTIONS = ["Archery", "Muzzleloader", "Rifle"].freeze
+    SPECIES_OPTIONS = %w[Elk Bear Deer Antelope].freeze
+    SEX_OPTIONS = %w[M F E].freeze
+    SEX_LABELS = { "M" => "Male", "F" => "Female", "E" => "Either" }.freeze
+    METHOD_OPTIONS = %w[Archery Muzzleloader Rifle].freeze
+    RESIDENT_OPTIONS = [["Resident", "true"], ["Non-Resident", "false"]].freeze
 
     validates :species, inclusion: { in: SPECIES_OPTIONS }, allow_blank: true
     validates :sex,     inclusion: { in: SEX_OPTIONS }, allow_blank: true
-    # validates :season,  inclusion: { in: SEASON_OPTIONS }, allow_blank: true
     validates :hunt_method,  inclusion: { in: METHOD_OPTIONS }, allow_blank: true
+
+    # Helper for select dropdowns with display labels
+    def self.sex_options_for_select
+      SEX_OPTIONS.map { |code| [SEX_LABELS[code], code] }
+    end
 end
